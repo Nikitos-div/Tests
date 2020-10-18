39f29352775b80a9bcbf491c270610e16cb88e5e
Nikita Churakov <52825986+Nikitos-div@users.noreply.github.com>
2020-10-18 23:40:20 +0300

    Update testA6.js

--- Corrections/testA6.js
+++ Corrections/testA6.js
@@ -1,6 +1,6 @@
 let test = string1 => {
-    let arr = [...string1]
-    arr.forEach((item, i) => { 
+    //let arr = [...string1]
+    [...string1].forEach((item, i) => { 
         return  console.log(i,",",string1[i])
     });
     
